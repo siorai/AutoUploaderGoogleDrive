22417ab76fd4f558cdc901b5e237013a43dd8c98
Paul Waldorf <siorai@gmail.com>
2016-12-09 10:17:00 -0800

    Added requirements of package to installer

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ setup(
     version='0.0.1dev',
     packages=find_packages(),
     long_description=open('README.md').read(),
+    install_requires=[
+        "google-api-python-client >= 1.2",
+        "oauth2client >= 4.0.0",
+        "PyYAML >= 3.0",
+        "httplib2",
+        "pydrive",
+    ]
     entry_points={
       'console_scripts': [
           'AutoUploaderGoogleDrive = AutoUploaderGoogleDrive:main',
