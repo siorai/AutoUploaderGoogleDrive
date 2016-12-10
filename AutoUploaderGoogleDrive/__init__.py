c7be1fd2059ac3a9fb46e5d06cb0db3ae0ad07be
Paul Waldorf <siorai@gmail.com>
2016-12-09 20:15:51 -0800

    Added logging initialization

--- AutoUploaderGoogleDrive/__init__.py
+++ AutoUploaderGoogleDrive/__init__.py
@@ -1,6 +1,15 @@
 from AutoUploaderGoogleDrive.emailtest import main, encode_message, send_message, get_filepaths
 from AutoUploaderGoogleDrive.upload import upload
 from AutoUploaderGoogleDrive.temp import setup_temp_file, addentry, finish_html
+from AutoUploaderGoogleDrive.settings import logfile
+
+import logging
+
 
 __version__ = '0.0.1'
 __author__ = 'Paul Waldorf'
+
+logging.basicConfig(filename=logfile,level=logging.DEBUG,format='%(asctime)s %(message)s')
+
+logging.info('AutoUploaderGoogleDrive initilized.')
+
