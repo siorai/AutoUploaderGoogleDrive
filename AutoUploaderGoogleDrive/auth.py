d4176bd781041f9eb85b63006a36df78c4948c76
Paul <siorai@gmail.com>
2016-12-11 19:16:56 -0500

    initial commit for emailtest2.py utilizing auth.py
    Also modified auth.py to have abundant debug logging
    in order to trace problems with acquiring data from settings
    
    Something tells me I'll probably just exchange these for a better
    more elaborate exception raising system once I learn how to impliment
    that, but for now this will do the trick.
    
    I'm also currently looking into being able to make a settings.py
    initial setup and testing script to help users with setting up the
    requirements for getting this script off the ground like creating
    your own google project and creating credentials.
    
    The changes implemented here in this commit were being able to run my
    existing service level auth flow from a different script, along with
    creating a function that will fetch which flow to use from the settings.
    
    I kept emailtest2.py and emailtest.py as seperate files so I could easily
    test both of them side by side to help with troubleshooting.

--- AutoUploaderGoogleDrive/auth.py
+++ AutoUploaderGoogleDrive/auth.py
@@ -1,6 +1,56 @@
-from oauth2client.client import flow_from_clientsecrets()
-from oauth2client.client import OAuth2WebServerFlow()
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.service_account import ServiceAccountCredentials
+from apiclient import discovery
+import httplib2, logging, os
+import settings
 
-from AutoUploaderGoogleDrive import flow_to_use, logfile, 
+httplib2.debuglevel = 4
 
+logging.basicConfig(filename=settings.logfile,level=logging.DEBUG,format='%(asctime)s %(message)s')
 
+"""
+Class for creation of an authorized credential object
+
+  Returns:
+    Authorized credentials object for api call
+"""
+    
+
+def Authorize():
+	
+    """
+    Fetches correct flow_to_use and builds accordingly
+    """
+
+    logging.debug('DEBUG: Authorization initialized')
+    flow = settings.flow_to_use
+
+    if(flow == "ServiceAccountFlow"):
+      logging.debug("DEBUG: FLOW: Service Account Selected!")
+      http = Service_Account_Credential()
+      return http
+    elif(flow == "Oauth2JSONFlow"):
+      print("Oauth2JSON selected!")
+    elif(flow == "Oauth2WebFlow"):
+      print("Oauth2WebFlow selected!")
+    else:
+      print("Nothing found!")
+   
+def Service_Account_Credential():
+	
+    logging.debug('DEBUG: SAC_FLOW: Attemping to load settings file from %s' % settings.servicekeyfile)
+    keyfile = settings.servicekeyfile
+    logging.debug('DEBUG: SAC_FLOW: Finished loading Service Account Keyfile: using %s' % keyfile )
+    logging.debug('DEBUG: SAC_FLOW: Loading scopes from settings file')
+    scopes = settings.scopes
+    logging.debug('DEBUG: SAC_FLOW: Finished loading scopes from settings file')
+    logging.debug('DEBUG: SAC_FLOW: Initializing credential from oauth2client.service_account')
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(keyfile, 
+                        scopes=scopes)
+    logging.debug('Delegating credentials from settings')
+    delegated_credentials = credentials.create_delegated(settings.delegated_email)
+    logging.debug('Initializing authorized, delegated credentials object')
+    http = delegated_credentials.authorize(httplib2.Http())
+    
+    return http
