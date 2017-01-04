07738b4371f70ba76a7e522f88d3cf757177fe4f
Paul <siorai@gmail.com>
2017-01-04 01:09:27 -0500

    Updated setup to reflect required packages changes

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='AutoUploaderGoogleDrive',
-    version='0.0.1dev',
+    version='0.0.3dev',
     packages=find_packages(),
     long_description=open('README.md').read(),
     install_requires=[
         "google-api-python-client >= 1.2",
         "oauth2client >= 4.0.0",
-        "httplib2",
-        "pydrive",
+        "httplib2"
+        "rarfile,
     ],
     entry_points={
       'console_scripts': [
           'AutoUploaderGoogleDrive = AutoUploaderGoogleDrive:main',
+          'Upload2GDrive = AutoUploaderGoogleDrive.CommandLine:main'
       ]
     }
 )
