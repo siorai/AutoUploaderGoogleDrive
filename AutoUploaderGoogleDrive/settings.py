1a8cd7a6e9ccaddf382e6b75064b050169e56fa0
Paul <siorai@gmail.com>
2017-01-04 00:45:14 -0500

    Added many new settings.
    Again, lacking on the documentation, apologies.
    
    Most of the changes were to make it easier for the user to change various settings
    in the script in order to facilitate easy of use, the CategoriesDictSetting being
    the exception to that.
    
    I'll be adding a bit of info about that and the Rules.py in a wiki coming up soon.

--- AutoUploaderGoogleDrive/settings.py
+++ AutoUploaderGoogleDrive/settings.py
@@ -1,4 +1,7 @@
-""":
+import logging
+import httplib2
+import os
+"""
 Settings script containing all account related variables
 and customization options as they are become created
 through development.
@@ -18,60 +21,122 @@ indicated by 'flow_to_use'
 """
 
 __author__ = 'siorai@gmail.com (Paul Waldorf)'
+#########################
+#[Transmission settings]#
+#########################
 
+torrentFileDirectory = "/path/to/torrent/directory/"
 
+###########################
+#[Remote sorting settings]#
+###########################
+
+categoriesDictSettings = {
+    'Music': { 
+        'Folder_ID': ['somefolder'],
+        'Matches': {
+            'Match_Tracker': ['sometracker']                          ],
+            'Match_Content_Extention' : [ 
+                    '*.aac', 
+                    '*.flac', 
+                    '*.mp3'
+            ]
+        }
+    },
+    'TV':   {
+        'Folder_ID': ['somefolder'],
+        'Matches': {
+            'Match_Tracker': ['sometracker'],
+            'Match_Expression': [
+                '*.S??E??.*', 
+                '*.s??e??.*'
+            ]
+
+        }
+    },
+    'Movies':{
+        'Folder_ID': ['somefolder'],
+        'Matches': {
+            'Match_Tracker': ['sometracker']
+        }
+    },
+    'XXX': {  
+        'Folder_ID': ['somefolder'],
+        'Matches': {
+            'Match_Tracker' : ['sometracker']
+        }
+    }
+}
 
 
 ###################
 #[Shared settings]#
 ###################
 
-flow_to_use = "ServiceAccountFlow" #choices are "ServiceAccountFlow", "Oauth2JSONFlow", or "Oauth2WebFlow"
-scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/gmail.compose']
-logfile = "./AutoUpload.log"
+flow_to_use = "Oauth2JSONFlow" 
+# choices are:
+# "ServiceAccountFlow" ( Implemented )  
+# "Oauth2JSONFlow" ( Implemented )
+# "Oauth2WebFlow" ( Not Implemented ) 
+
+scopes =    [
+                'https://www.googleapis.com/auth/drive', 
+                'https://www.googleapis.com/auth/gmail.compose',
+                'https://www.googleapis.com/auth/urlshortener'
+            ]
 redirect_uri = "http://example.com/auth_return"
-googledrivedir = ['googlefolderidhere']
+googledrivedir = ['defaultgoogledrivedirhere']
+
+####################
+#[Logging settings]#
+####################
+
+logfile = "/path/to/log/file.log"
+loggingSetup = logging.basicConfig(filename=logfile,level=logging.DEBUG,format='%(asctime)s %(message)s')
+httplib2.debuglevel = 4
+
+
+################################
+#[Google Drive Upload Settings]#
+################################
+
+nonDefaultPermissions = True
+permissionValue = 'anyone'
+permissionType = 'anyone'
+permissionRole = 'reader'
+
+chunksize = 50000*1024 #Uploading Chunksize
+
 
 ##################
 #[Email settings]#
 ##################
 
-emailparameters = ['createdDate', 'title', 'alternateLink', 'md5Checksum', 'id', 'fileSize' ]
-"""
-Parameters to include in the email table. Customize as you see fit.
-"""
-emailSender = "email"
-emailTo = "email"
+emailparameters = ['title', 'md5Checksum', 'id', 'alt_tiny', 'fileSize']
+emailSender = "some@email.com"
+emailTo = "some@email.com"
 deleteTmpHTML = False
-
+tempfilename = '/path/to/temp/html/file.html' % os.getpid()
 
 ###############################
 #[Service Account Credentials]#
 ###############################
 
-#path to the local json that stores actual credential information
-servicekeyfile = "json"
-
-#client_email from Google Developers Console
-client_email = "gserviceaccount.com"
-
-#user_email for delegation
-delegated_email = ""
+servicekeyfile = "/path/to/servicekey.json"
+client_email = "someprojectname@blahblah.gserviceaccount.com"
+delegated_email = "userdata@access.com"
 
 ############################
 #[Normal JSON Oauth2 Creds]#
 ############################
 
-
-
-#path to normal oauth2 json credentials file
-oauth2keyfile = "json"
-
-
+pickledcredsFile = "/path/to/pickledcredsFile"
+oauth2keyfile = "/path/to/oauth2key.json"
+redirect_uri = 'urn:ietf:wg:oauth:2.0:oob' 
 
 #################################
 #[Normal WebServer Oauth2 Creds]#
 #################################
 
-oauth2web_id = ""
-oauth2web_secret = ""
+oauth2web_id = "randomintblahblah.apps.googleusercontent.com"
+oauth2web_secret = "oauth2web_secret"
